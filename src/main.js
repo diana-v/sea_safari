dce5b94b4d6141fd2e692084eda390ad386c6bd9
Diana V <d.valaityte.97@gmail.com>
2020-02-12 19:13:22 +0200

    Created a grid layout for offers page.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
--- src/views/Offers.vue
+++ src/views/Offers.vue
@@ -1,115 +1,229 @@
 <template>
-  <div class="offers">
-    <div class="blog-wrap">
-      <div class="blog-container">
-        <v-card link hover
-                class="blog-card">
-          <v-img
-                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                  class="white--text align-end"
-                  height="200px"
-          >
-          </v-img>
-          <v-card-title class="blog-card-title">Ekstremali išvyka į jūrą</v-card-title>
-          <v-card-text class="text--primary">
-            <div>Šis ekstremalus plaukimas Baltijos jūra su kateriu įsimins ilgam! Greitaeigis R.I.B. kateris ir
-              audringa Baltijos jūra nenuvils ir didžiausio adrenalino fanatiko. Kelionės metu turėsite galimybe
-              praplaukti Danės upe, pamatyti Baltijos jūros vartus. Skrodžianti Baltijos jūros bangas pajausite visą
-              jūros galybę, greitį, vėją ir vandens purslus.</div>
-          </v-card-text>
-          <div class="time-cost-wrapper">
-            <div class="column">
-              <v-card-text class="offers-time-cost">Laikas:</v-card-text>
-              <v-card-text class="offers-time-cost"><i class="fas fa-clock"></i> 30min</v-card-text>
-            </div>
-            <div class="column">
-              <v-card-text class="offers-time-cost">Nuo:</v-card-text>
-              <v-card-text class="offers-time-cost"><i class="fas fa-euro-sign"></i> 15</v-card-text>
-            </div>
-            <div class="column">
-              <v-btn>Daugiau</v-btn>
-            </div>
-          </div>
-        </v-card>
-      </div>
+<div class="container">
+  <div class="row display-flex">
+    <div class="col-md-6">
+        <div class="card">
+        <div class="card-img-body">
+            <img class="card-img" src="https://picsum.photos/500/230" alt="Card image cap">
+        </div>
+        <div class="card-body">
+            <h4 class="card-title">Ekstremali išvyka į jūrą</h4>
+            <p class="card-text offer-text">Šis ekstremalus plaukimas Baltijos jūra su kateriu įsimins ilgam! Greitaeigis R.I.B. kateris ir audringa Baltijos jūra nenuvils ir didžiausio adrenalino fanatiko. Kelionės metu turėsite galimybe praplaukti Danės upe, pamatyti Baltijos jūros vartus. Skrodžianti Baltijos jūros bangas pajausite visą jūros galybę, greitį, vėją ir vandens purslus.</p>
+            <div class="col-sm offer-details"><i class="fas fa-clock"></i> 30min</div>
+            <div class="col-sm offer-details">Nuo: <i class="fas fa-euro-sign"></i> 15</div>
+            <div class="col-sm offer-details"><a href="#" class="btn btn-outline-primary offer-button">Daugiau</a></div>
+        </div>
+        </div>
     </div>
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-img-body">
+                                    <img class="card-img" src="https://picsum.photos/500/230" alt="Card image cap">
+                                </div>
+                                <div class="card-body">
+                                    <h4 class="card-title">Išvyka į Mingės kaimą</h4>
+                                    <p class="card-text offer-text">Išvykos metu, pamatysite Lietuvos Venecija vadinamą kaimą, kuris yra ypač gražus ir unikalus, o gatvę jame atstoja upė. Tai Mingės kaimas, vieta kurią kiekvienas privalo aplankyti!</p>
+                                    <div class="col-sm offer-details"><i class="fas fa-clock"></i> 4-6val</div>
+                                    <div class="col-sm offer-details">Nuo: <i class="fas fa-euro-sign"></i> 400</div>
+                                    <div class="col-sm offer-details"><a href="#" class="btn btn-outline-primary offer-button">Daugiau</a></div>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6 display-flex"><div class="card">
+                            <div class="card-img-body">
+                                <img class="card-img" src="https://picsum.photos/500/230" alt="Card image cap">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Išvyka į Juodkrantę</h4>
+                                <p class="card-text offer-text">Išplaukę iš Klaipėdos apiplauksite uosto akvatorija, grožėsities unikaliu Kuršių nerijos kraštovaizdžiu, vėju supūstytomis kopomis. Neveltui šis nedidelis, ramus ir tvarkingas kurortas yra istorinis - urbanistinis draustinis. Kelionė laivu leis grožėtis Juodkrante iš vandens, o tai paliks tik dar daugiau neišdildomų įspūdžių.</p>
+                                <div class="col-sm offer-details"><i class="fas fa-clock"></i> 2-4val</div>
+                                <div class="col-sm offer-details">Nuo: <i class="fas fa-euro-sign"></i> 200</div>
+                                <div class="col-sm offer-details"><a href="#" class="btn btn-outline-primary offer-button">Daugiau</a></div>
+                            </div>
+                        </div></div>
+                        <div class="col-md-6"><div class="card">
+                            <div class="card-img-body">
+                                <img class="card-img" src="https://picsum.photos/500/230" alt="Card image cap">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Ekstremali išvyka į jūrą</h4>
+                                <p class="card-text offer-text">Šis ekstremalus plaukimas Baltijos jūra su kateriu įsimins ilgam! Greitaeigis R.I.B. kateris ir audringa Baltijos jūra nenuvils ir didžiausio adrenalino fanatiko. Kelionės metu turėsite galimybe praplaukti Danės upe, pamatyti Baltijos jūros vartus. Skrodžianti Baltijos jūros bangas pajausite visą jūros galybę, greitį, vėją ir vandens purslus.</p>
+                                <div class="col-sm offer-details"><i class="fas fa-clock"></i> 30min</div>
+                                <div class="col-sm offer-details">Nuo: <i class="fas fa-euro-sign"></i> 15</div>
+                                <div class="col-sm offer-details"><a href="#" class="btn btn-outline-primary offer-button">Daugiau</a></div>
+                            </div>
+                        </div></div>
+                      </div>
+                      <div class="row display-flex">
+                        <div class="col-md-6"><div class="card">
+                            <div class="card-img-body">
+                                <img class="card-img" src="https://picsum.photos/500/230" alt="Card image cap">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Ekstremali išvyka į jūrą</h4>
+                                <p class="card-text offer-text">Šis ekstremalus plaukimas Baltijos jūra su kateriu įsimins ilgam! Greitaeigis R.I.B. kateris ir audringa Baltijos jūra nenuvils ir didžiausio adrenalino fanatiko. Kelionės metu turėsite galimybe praplaukti Danės upe, pamatyti Baltijos jūros vartus. Skrodžianti Baltijos jūros bangas pajausite visą jūros galybę, greitį, vėją ir vandens purslus.</p>
+                                <div class="col-sm offer-details"><i class="fas fa-clock"></i> 30min</div>
+                                <div class="col-sm offer-details">Nuo: <i class="fas fa-euro-sign"></i> 15</div>
+                                <div class="col-sm offer-details"><a href="#" class="btn btn-outline-primary offer-button">Daugiau</a></div>
+                            </div>
+                        </div></div>
+                        <div class="col-md-6">
+                            <div class="card">
+                            <div class="card-img-body">
+                                <img class="card-img" src="https://picsum.photos/500/230" alt="Card image cap">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Ekstremali išvyka į jūrą</h4>
+                                <p class="card-text offer-text">Šis ekstremalus plaukimas Baltijos jūra su kateriu įsimins ilgam! Greitaeigis R.I.B. kateris ir audringa Baltijos jūra nenuvils ir didžiausio adrenalino fanatiko. Kelionės metu turėsite galimybe praplaukti Danės upe, pamatyti Baltijos jūros vartus. Skrodžianti Baltijos jūros bangas pajausite visą jūros galybę, greitį, vėją ir vandens purslus.</p>
+                                <div class="col-sm offer-details"><i class="fas fa-clock"></i> 30min</div>
+                                <div class="col-sm offer-details">Nuo: <i class="fas fa-euro-sign"></i> 15</div>
+                                <div class="col-sm offer-details"><a href="#" class="btn btn-outline-primary offer-button">Daugiau</a></div>
+                            </div>
+                        </div></div>
+                      </div>
   </div>
 </template>
 
-
 <style>
-  .time-cost-wrapper {
-    display: grid;
-    grid-template-rows: 1fr 1fr;
-    grid-template-columns: 1fr 1fr 1fr;
-    height: 125px;
-  }
-  .column {
-    float: left;
-    grid-column: 1fr;
-    padding: 10px;
-  }
-
-  .offers-time-cost {
-    padding-bottom: 0px;
-    font-size: 1.25rem;
-  }
-
-  .offers {
-    text-align: start;
-  }
-
-  .blog-container {
-    float: left;
-    margin: 20px 5%;
-    width: 40%;
-  }
-
-  @media screen and (max-width: 825px) {
-    .blog-container {
-      width: 80%;
-      margin: 0 auto;
-      padding: 20px;
-      float: unset;
+    .offer-text {
+        text-align: start;
     }
-  }
-
-  @media screen and (min-width: 1300px) {
-    .blog-container {
-      width: 30%;
-      margin: 0 20px;
-      padding: 20px;
-      /*float: unset;*/
+    .offer-button {
+        float: right;
     }
-  }
 
-  .blog-card {
-    text-decoration: none !important;
-    width: 600px;
-  }
+    div.col-sm:nth-child(5) {
+        padding-top: 6px;
+    }
 
-  .blog-card-title {
-    color: #0c3c60;
-  }
-
-  .blog-card-title:hover {
-    color: #7f244b !important;
-  }
-
-  .blog-card-subtitle {
-    border-bottom: 3px solid #7f244b;
-    width: 150px;
-  }
-
-  .blog-wrap:hover .blog-card {
-    opacity: .3;
-  }
-
-  .blog-wrap .blog-card:hover {
-    opacity: 1;
-  }
-
-  .offers-cost-time {
-    padding: none !important;
-  }
+    .offer-details {
+        max-width: 30%;
+        float: left;
+    }
+    .row.display-flex {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .row.display-flex > [class*='col-'] {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
+                    <!--<template>-->
+<!--  <div class="offers">-->
+<!--    <div class="blog-wrap">-->
+<!--      <div class="blog-container">-->
+<!--        <v-card link hover-->
+<!--                class="blog-card">-->
+<!--          <v-img-->
+<!--                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"-->
+<!--                  class="white&#45;&#45;text align-end"-->
+<!--                  height="200px"-->
+<!--          >-->
+<!--          </v-img>-->
+<!--          <v-card-title class="blog-card-title">Ekstremali išvyka į jūrą</v-card-title>-->
+<!--          <v-card-text class="text&#45;&#45;primary">-->
+<!--            <div>Šis ekstremalus plaukimas Baltijos jūra su kateriu įsimins ilgam! Greitaeigis R.I.B. kateris ir-->
+<!--              audringa Baltijos jūra nenuvils ir didžiausio adrenalino fanatiko. Kelionės metu turėsite galimybe-->
+<!--              praplaukti Danės upe, pamatyti Baltijos jūros vartus. Skrodžianti Baltijos jūros bangas pajausite visą-->
+<!--              jūros galybę, greitį, vėją ir vandens purslus.</div>-->
+<!--          </v-card-text>-->
+<!--          <div class="time-cost-wrapper">-->
+<!--            <div class="column">-->
+<!--              <v-card-text class="offers-time-cost">Laikas:</v-card-text>-->
+<!--              <v-card-text class="offers-time-cost"><i class="fas fa-clock"></i> 30min</v-card-text>-->
+<!--            </div>-->
+<!--            <div class="column">-->
+<!--              <v-card-text class="offers-time-cost">Nuo:</v-card-text>-->
+<!--              <v-card-text class="offers-time-cost"><i class="fas fa-euro-sign"></i> 15</v-card-text>-->
+<!--            </div>-->
+<!--            <div class="column">-->
+<!--              <v-btn>Daugiau</v-btn>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </v-card>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+
+<!--<style>-->
+<!--  .time-cost-wrapper {-->
+<!--    display: grid;-->
+<!--    grid-template-rows: 1fr 1fr;-->
+<!--    grid-template-columns: 1fr 1fr 1fr;-->
+<!--    height: 125px;-->
+<!--  }-->
+<!--  .column {-->
+<!--    float: left;-->
+<!--    grid-column: 1fr;-->
+<!--    padding: 10px;-->
+<!--  }-->
+
+<!--  .offers-time-cost {-->
+<!--    padding-bottom: 0px;-->
+<!--    font-size: 1.25rem;-->
+<!--  }-->
+
+<!--  .offers {-->
+<!--    text-align: start;-->
+<!--  }-->
+
+<!--  .blog-container {-->
+<!--    float: left;-->
+<!--    margin: 20px 5%;-->
+<!--    width: 40%;-->
+<!--  }-->
+
+<!--  @media screen and (max-width: 825px) {-->
+<!--    .blog-container {-->
+<!--      width: 80%;-->
+<!--      margin: 0 auto;-->
+<!--      padding: 20px;-->
+<!--      float: unset;-->
+<!--    }-->
+<!--  }-->
+
+<!--  @media screen and (min-width: 1300px) {-->
+<!--    .blog-container {-->
+<!--      width: 30%;-->
+<!--      margin: 0 20px;-->
+<!--      padding: 20px;-->
+<!--      /*float: unset;*/-->
+<!--    }-->
+<!--  }-->
+
+<!--  .blog-card {-->
+<!--    text-decoration: none !important;-->
+<!--    width: 600px;-->
+<!--  }-->
+
+<!--  .blog-card-title {-->
+<!--    color: #0c3c60;-->
+<!--  }-->
+
+<!--  .blog-card-title:hover {-->
+<!--    color: #7f244b !important;-->
+<!--  }-->
+
+<!--  .blog-card-subtitle {-->
+<!--    border-bottom: 3px solid #7f244b;-->
+<!--    width: 150px;-->
+<!--  }-->
+
+<!--  .blog-wrap:hover .blog-card {-->
+<!--    opacity: .3;-->
+<!--  }-->
+
+<!--  .blog-wrap .blog-card:hover {-->
+<!--    opacity: 1;-->
+<!--  }-->
+
+<!--  .offers-cost-time {-->
+<!--    padding: none !important;-->
+<!--  }-->
+<!--</style>-->
+
