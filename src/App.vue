c480f20bc3c96601100e3116eceb56022ff361d8
Diana V <d.valaityte.97@gmail.com>
2020-03-05 12:57:20 +0200

    Removed facebook integration from index.html. Fixed spelling mistake on offers section.

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,4 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
   }
-
-  .app {
-    /*background-image: url("https://scontent.fvno2-1.fna.fbcdn.net/v/t1.0-9/59979457_1078530229013606_565074729577218048_n.jpg?_nc_cat=101&_nc_ohc=KM1XAHtrACYAX-FOKU8&_nc_ht=scontent.fvno2-1.fna&oh=35142619fc576dc7c473550a8c05b330&oe=5EC732C6");  -webkit-background-size: cover;*/
-    /*-moz-background-size: cover;*/
-    /*-o-background-size: cover;*/
-    /*background-size: cover;*/
-    /*height: 100%;*/
-    /*overflow: hidden;*/
-  }
 </style>
