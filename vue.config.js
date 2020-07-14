09ec5f0dc4c470b70f69ba80a9bb65ffab311077
Diana V <d.valaityte.97@gmail.com>
2020-07-14 16:38:00 +0300

    Added PWA favicons to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,36 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    pwa: {
+        manifestOptions: {
+            icons: [
+                {
+                    'src': 'static/favicons/android-chrome-192x192.png',
+                    'sizes': '192x192',
+                    'type': 'image/png',
+                },
+                {
+                    'src': 'static/favicons/apple-chrome-180x180.png',
+                    'sizes': '180x180',
+                    'type': 'image/png',
+                },
+                {
+                    'src': 'static/favicons/favicon-16x16.png',
+                    'sizes': '16x16',
+                    'type': 'image/png',
+                },
+                {
+                    'src': 'static/favicons/favicon-32x32.png',
+                    'sizes': '32x32',
+                    'type': 'image/png',
+                },
+                {
+                    'src': 'static/favicons/favicon-96x96.png',
+                    'sizes': '96x96',
+                    'type': 'image/png',
+                },
+            ]
+        }
+    }
+};
